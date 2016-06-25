2c8330ba02077824e0b957144f5d3ba1fefd47e3
Bobstin <justinakahn@gmail.com>
2016-06-25 10:49:10 -0400

    Removes diagnostic email printing

--- TravelMonitor.py
+++ TravelMonitor.py
@@ -122,9 +122,9 @@ def SendGrid_Email(user,recipient,subject,body):
 	content = Content("text/plain",body)
 	mail = Mail(from_email,subject,to_email,content)
 	response = sg.client.mail.send.post(request_body=mail.get())
-	print(response.status_code)
-	print(response.body)
-	print(response.headers)
+	#print(response.status_code)
+	#print(response.body)
+	#print(response.headers)
 
 
 try:

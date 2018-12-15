b9d4a6c415c96a92230a503251adb2c2b9c72be4
Bobstin <justinakahn@gmail.com>
2018-12-15 18:18:54 -0500

    Adds pdp monitoring

--- TDFMonitor.py
+++ TDFMonitor.py
@@ -9,14 +9,17 @@ import sendgrid
 from sendgrid.helpers.mail import Email, Content, Mail
 import requests
 import json
+from string import Template
 
 #GmailPass = os.environ.get('GmailPass')
 DBPass = os.environ.get('DBPass')
 TDFUsername = os.environ.get('TDFUsername')
 TDFPass = os.environ.get('TDFPass')
 DatabaseURL = os.environ.get('DATABASE_URL')
-SendGridAPIKey=os.environ.get('SENDGRID_API_KEY')
-TimePastHour=os.environ.get('TimePastHour')
+SendGridAPIKey = os.environ.get('SENDGRID_API_KEY')
+TimePastHour = os.environ.get('TimePastHour')
+PBP_username = os.environ.get('PBPUsername')
+PBP_password = os.environ.get('PBPPass')
 
 def sendgrid_email(user,recipient,subject,body):
     sg = sendgrid.SendGridAPIClient(apikey=SendGridAPIKey)
@@ -27,12 +30,12 @@ def sendgrid_email(user,recipient,subject,body):
     response = sg.client.mail.send.post(request_body=mail.get())
 
 
-def detect_new_shows(new_shows):
+def detect_new_shows(new_shows, website):
     print ("Checking for new shows")
     
     #Connects to the database, creates a cursor
     if DatabaseURL=='127.0.0.1':
-        conn = psycopg2.connect(database="monitordb",user="tdfmonitor",password=DBPass,host=DatabaseURL,port="5432")
+        conn = psycopg2.connect(database="monitordb",user="postgres",password=DBPass,host=DatabaseURL,port="5432")
     else:
         urlparse.uses_netloc.append("postgres")
         url = urlparse.urlparse(os.environ["DATABASE_URL"])
@@ -49,7 +52,7 @@ def detect_new_shows(new_shows):
     cur = conn.cursor()
         
     #clears the previous pull
-    cur.execute("DELETE FROM latest_tdf_pull;")
+    cur.execute(f"DELETE FROM latest_{website}_pull;")
 
     #Sets the the line for how long a show needs to be missing before sending
     currenttime = datetime.datetime.now()
@@ -59,52 +62,51 @@ def detect_new_shows(new_shows):
 
     #inserts the latest pull into the database
     for show in new_shows:
-        #print show.text
-        cur.execute("INSERT INTO latest_tdf_pull(show_name,last_found) VALUES (%s,%s);",[show,datetime.datetime.now()])
+        show = show.replace("'","''") # Needed to escape single quotes
+        cur.execute(f"INSERT INTO latest_{website}_pull(show_name,last_found) VALUES ('{show}','{datetime.datetime.now()}');")
 
     #compares the two tables to identify what is new
-    cur.execute("""SELECT latest_tdf_pull.show_name FROM latest_tdf_pull LEFT OUTER JOIN tdf_shows ON (latest_tdf_pull.show_name = tdf_shows.show_name)
-    WHERE tdf_shows.show_name is null
-    OR tdf_shows.last_found<%s;""",[oldline])
-    showstosend = cur.fetchall()
-    #print showstosend 
+    cur.execute(f"""SELECT latest_{website}_pull.show_name FROM latest_{website}_pull LEFT OUTER JOIN {website}_shows ON (latest_{website}_pull.show_name = {website}_shows.show_name)
+    WHERE {website}_shows.show_name is null
+    OR {website}_shows.last_found<'{oldline}';""")
+    shows_to_send = cur.fetchall()
 
     #Adds the missing rows
-    cur.execute("""INSERT INTO tdf_shows
-    SELECT latest_tdf_pull.show_name FROM latest_tdf_pull LEFT OUTER JOIN tdf_shows ON (latest_tdf_pull.show_name = tdf_shows.show_name)
-    WHERE tdf_shows.show_name is null""")
+    cur.execute(f"""INSERT INTO {website}_shows
+    SELECT latest_{website}_pull.show_name FROM latest_{website}_pull LEFT OUTER JOIN {website}_shows ON (latest_{website}_pull.show_name = {website}_shows.show_name)
+    WHERE {website}_shows.show_name is null""")
     
     #updates the tdf_shows table with the new last_found times
-    cur.execute("""UPDATE tdf_shows
-    SET last_found = latest_tdf_pull.last_found
-    FROM latest_tdf_pull
-    WHERE tdf_shows.show_name = latest_tdf_pull.show_name;""")
+    cur.execute(f"""UPDATE {website}_shows
+    SET last_found = latest_{website}_pull  .last_found
+    FROM latest_{website}_pull
+    WHERE {website}_shows.show_name = latest_{website}_pull.show_name;""")
 
     #Commits the changes to the database
     conn.commit()
 
-    if len(showstosend) > 0:
+    if len(shows_to_send) > 0:
         #starts to draft the email
-        emailbody ="TDF Monitor has found a new show is being offered on TDF:\n\n"
+        email_body =f"{website} Monitor has found a new show is being offered on {website}:\n\n"
 
         #Adds list of shows that were found
-        for show in showstosend:
-            emailbody = emailbody + "\t" + show[0] + "\n"
+        for show in shows_to_send:
+            email_body = email_body + "\t" + show[0] + "\n"
             print(show[0])
 
-        emailbody = emailbody + "\nBest,\nTDF Monitor"
+        email_body = email_body + f"\nBest,\n{website} Monitor"
 
         #queries the database for the list of emails of users that want tdf updates
-        cur.execute("""SELECT email FROM users WHERE want_tdf ='t' ;""")
+        cur.execute(f"""SELECT email FROM users WHERE want_{website} ='t' ;""")
         emails = cur.fetchall()
 
         #Sends the email
         #print emailbody
         for email in emails:
-            sendgrid_email("tdfmonitor@gmail.com",email[0],"New show detected by TDF Monitor",emailbody)
+            sendgrid_email("tdfmonitor@gmail.com", email[0], f"New show detected by {website} Monitor", email_body)
         
     else:
-        print("No new shows detected")
+        print(f"No new shows detected on {website}")
 
 
 def TDF_pull():
@@ -135,9 +137,23 @@ def TDF_pull():
             if keyword['categoryName'] == 'Venue' and keyword['keywordName'] == 'Broadway':
                 broadway_shows.append(show['title'])
 
-    print(broadway_shows)
-
     return broadway_shows
+
+
+def pbp_pull():
+    with requests.Session() as session:
+        data = {'email': PBP_username, 'password': PBP_password}
+        login = session.post('https://www.play-by-play.com/login/action', data=data)
+        soup = bs4.BeautifulSoup(login.text, features='lxml')
+        shows = soup.find_all('div',{'class': 'product-content'})
+        shows = [show.find('div',{'class': 'product-content__title'}).text.strip()
+                 for show in shows
+                 if show.find('div',{'class': 'product-content__title'}) is not None
+                 and 'broadway' in show.find('div',{'class': 'product-content__meta'}).text.strip().lower()]
+
+    return shows
+
+
 
 
 def wait_one_hour():
@@ -156,11 +172,13 @@ if __name__ == '__main__':
         timestamp = time.strftime("%m/%d/%y %H:%M:")
         print (timestamp.encode('utf-8'))
 
-        #Pulls the HTML from the TDF website
-        new_shows = TDF_pull()
+        #Pulls the HTML from the TDF and PBP websites
+        new_tdf_shows = TDF_pull()
+        new_pbp_shows = pbp_pull()
 
         #Compares the current and previous shows
-        detect_new_shows(new_shows)
+        detect_new_shows(new_tdf_shows, 'TDF')
+        detect_new_shows(new_pbp_shows, 'PBP')
 
         #Waits for an hour to check again
         print ("Going back to sleep\n")
